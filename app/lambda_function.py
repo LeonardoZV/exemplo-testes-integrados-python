73d0e4372ef2b7835c738fb560882f139070990e
Leonardo Vasconcellos <leozvasconcellos@gmail.com>
2024-09-25 13:55:42 -0300

    wip

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -6,8 +6,12 @@ sns_client = boto3.client('sns')
 
 
 def lambda_handler(event, context):
-    AWS_REGION = os.environ.get("AWS_REGION", os.environ.get("AWS_DEFAULT_REGION", "us-east-1"))
+    
+    AWS_REGION = os.environ.get("AWS_REGION", os.environ.get("AWS_DEFAULT_REGION", "us-east-1"))    
     AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT_ID", "000000000000")
     topic_arn = f"arn:aws:sns:{AWS_REGION}:{AWS_ACCOUNT_ID}:teste"
-    response = sns_client.publish(TopicArn=topic_arn, Message=json.dumps(event))
-    return {'statusCode': 200, 'body': json.dumps(response, default=str)}
+
+    for record in event["Records"]:
+        sns_client.publish(TopicArn=topic_arn, Message=json.dumps(record["dynamodb"]["Keys"]))
+    
+    return {'statusCode': 200, 'body': 'Processamento Realizado'}
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,22 +5,41 @@ from testcontainers.localstack import LocalStackContainer
 from testcontainers.core.labels import LABEL_SESSION_ID, SESSION_ID
 
 
-def create_lambda_function(lambda_client, lambda_function_name):
-    lambda_folder_path = os.path.abspath(os.path.dirname(__file__).replace("tests", "app"))
-    response = lambda_client.create_function(
-        FunctionName=lambda_function_name,
-        Runtime='python3.12',
-        Role='arn:aws:iam::000000000000:role/lambda-role',
-        Handler='lambda_function.lambda_handler',
-        Code={'S3Bucket': 'hot-reload', 'S3Key': lambda_folder_path}
+def create_table(dynamodb_client, dynamodb_table_name):
+    response = dynamodb_client.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'pk',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'sk',
+                'AttributeType': 'S'
+            }
+        ],
+        TableName=dynamodb_table_name,
+        KeySchema=[
+            {
+                'AttributeName': 'pk',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'sk',
+                'KeyType': 'RANGE'
+            }
+        ],
+        BillingMode='PAY_PER_REQUEST',
+        StreamSpecification={
+            'StreamEnabled': True,
+            'StreamViewType': 'NEW_AND_OLD_IMAGES'
+        }
     )
-    lambda_client.get_waiter('function_active_v2').wait(FunctionName=lambda_function_name)
-    return response['FunctionArn']
+    return response
 
 
 def create_sns_topic(sns_client, sns_topic_name):
     response = sns_client.create_topic(Name=sns_topic_name)
-    return response['TopicArn']
+    return response
 
 
 def create_sqs_queue(sqs_client, queue_name):
@@ -33,11 +52,35 @@ def subscribe_sqs_to_sns(sns_client, topic_arn, queue_arn):
     sns_client.subscribe(TopicArn=topic_arn, Protocol='sqs', Endpoint=queue_arn)
 
 
+def create_lambda_function(lambda_client, lambda_function_name):
+    lambda_folder_path = os.path.abspath(os.path.dirname(__file__).replace("tests", "app"))
+    response = lambda_client.create_function(
+        FunctionName=lambda_function_name,
+        Runtime='python3.12',
+        Role='arn:aws:iam::000000000000:role/lambda-role',
+        Handler='lambda_function.lambda_handler',
+        Code={
+            'S3Bucket': 'hot-reload', 
+            'S3Key': lambda_folder_path
+        }
+    )
+    lambda_client.get_waiter('function_active_v2').wait(FunctionName=lambda_function_name)
+    return response
+
+
+def create_event_source_mapping(lambda_client, lambda_function_name, event_source_arn):
+    response = lambda_client.create_event_source_mapping(
+        FunctionName=lambda_function_name,
+        EventSourceArn=event_source_arn,
+        StartingPosition='TRIM_HORIZON'
+    )
+
+
 @pytest.mark.filterwarnings("ignore:datetime.datetime.utcnow")
 def test_lambda_function():
     # Arrange
     localstack = (LocalStackContainer(image="localstack/localstack:latest", region_name="us-east-1")
-                  .with_services("lambda", "sns", "sqs")
+                  .with_services("dynamodb", "lambda", "sns", "sqs")
                   .with_env("LAMBDA_RUNTIME_IMAGE_MAPPING", '{"python3.12": "public.ecr.aws/lambda/python:3.12"}')
                   # SESSION_ID NECESSARIO PARA QUE O LAMBDA CONTAINER SEJA EXCLUIDO AUTOMATICAMENTE.
                   # É UM BUG QUE FOI CONCERTADO NA LIB JAVA (https://github.com/localstack/localstack/issues/8616).
@@ -48,28 +91,43 @@ def test_lambda_function():
 
     with localstack as localstack:
         
-        lambda_client = localstack.get_client("lambda")
+        dynamodb_client = localstack.get_client("dynamodb")
         sns_client = localstack.get_client("sns")
         sqs_client = localstack.get_client("sqs")
+        lambda_client = localstack.get_client("lambda")
 
-        function_name = "teste"
+        table_name = "teste"
         topic_name = "teste"
         queue_name = "teste"
+        function_name = "teste"
 
-        create_lambda_function(lambda_client, function_name)
-        topic_arn = create_sns_topic(sns_client, topic_name)
+        stream_arn = create_table(dynamodb_client, table_name)["TableDescription"]["LatestStreamArn"]
+        topic_arn = create_sns_topic(sns_client, topic_name)['TopicArn']
         queue_url, sqs_queue_arn = create_sqs_queue(sqs_client, queue_name)
         subscribe_sqs_to_sns(sns_client, topic_arn, sqs_queue_arn)
+        create_lambda_function(lambda_client, function_name)['FunctionArn']
+        create_event_source_mapping(lambda_client, function_name, stream_arn)
 
-        input = {"bla": "blab"}
+        input = {
+            'pk': {
+                'S': 'A'
+            },
+            'sk': {
+                'S': 'B'
+            }
+        }
 
         # Act
-        lambda_response = lambda_client.invoke(FunctionName=function_name, Payload=json.dumps(input).encode())
+        put_item_response = dynamodb_client.put_item(
+            TableName=table_name,
+            Item=input
+        )
 
         sqs_response = sqs_client.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=5)
         messages = sqs_response.get('Messages', [])
         messages_dict = [json.loads(json.loads(message["Body"])["Message"]) for message in messages]
 
+        print(messages_dict)
+
         # Assert
-        assert lambda_response['StatusCode'] == 200
         assert input in messages_dict
